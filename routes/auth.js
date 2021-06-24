d8abc3441cb4d3ae884679aac3d37801ab3293f0
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-24 19:22:35 +0200

    added authentication route to validate user email && password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../models/User');
 
 // @route	->	GET api/auth
 // @descr	->	Get logged in user
@@ -12,8 +17,55 @@ router.get('/', (req, res) => {
 // @route	->	POST api/auth
 // @descr	->	auth user & get token
 // @access	->	Public
-router.post('/', (req, res) => {
-	res.send('Log in user');
+router.post('/', [
+	check('email', 'Please include a valid Email').isEmail(),
+	check('password', 'Password is required').exists()
+], async (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	const { email, password } = req.body;
+
+	try {
+		// Waiting to find User email in DB
+		let user = await User.findOne({ email });
+
+		// Sending err msg if wrong email
+		if (!user) {
+			return res.status(400).json({ msg: 'Invalid Credentials' });
+		}
+
+		// Comparing user registered password
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		// Sending err msg if wrong password
+		if (!isMatch) {
+			return res.status(400).json({ msg: 'Invalid Credentials' });
+		}
+
+		// payload obj to send user id through jsonwebtoken
+		const payload = {
+			user: {
+				id: user.id
+			}
+		}
+
+		// Generate a token
+		jwt.sign(payload, config.get('jwtSecret'), {
+			expiresIn: 360000
+		}, (err, token) => {
+			if (err) {
+				throw err;
+			}
+			res.json({ token });
+		});
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Sever Error');
+
+	}
 });
 
 module.exports = router;
