050a725ec036d7281b4f02648907b43884f9ae7b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-01 17:37:29 +0200

    added check localStorage condition to load main components loads from setAuthToken

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,13 @@ import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
@@ -22,7 +27,7 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className="container">
-              <Alerts />
+                <Alerts />
                 <Switch>
                   <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
