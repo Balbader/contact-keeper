55b4f99df1733f1f8d24098bafd4c0cfeb629274
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-03 09:02:49 +0200

    GET_CONTACTS case added to action.type

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,7 +1,8 @@
 import {
-	CONTACT_ERROR,
+	GET_CONTACTS,
 	ADD_CONTACT,
 	DELETE_CONTACT,
+	CONTACT_ERROR,
 	SET_CURRENT,
 	CLEAR_CURRENT,
 	UPDATE_CONTACT,
@@ -12,20 +13,29 @@ import {
 // eslint-disable-next-line
 export default (state, action) => {
 	switch (action.type) {
+		case GET_CONTACTS:
+			return {
+				...state,
+				contacts: action.payload,
+				loading: false
+			}
 		case ADD_CONTACT:
 			return {
 				...state,
-				contacts: [...state.contacts, action.payload]
+				contacts: [...state.contacts, action.payload],
+				loading: false
 			}
 		case UPDATE_CONTACT:
 			return {
 				...state,
-				contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+				contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact),
+				loading: false
 			}
 		case DELETE_CONTACT:
 			return {
 				...state,
-				contacts: state.contacts.filter((contact) => contact.id !== action.payload)
+				contacts: state.contacts.filter((contact) => contact.id !== action.payload),
+				loading: false
 			}
 		case SET_CURRENT:
 			return {
