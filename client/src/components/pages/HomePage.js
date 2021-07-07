b3a585632d9c9bb145d5c8c3c927e3b8db58c15d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-07 16:14:38 +0200

    change home page background pic

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -3,8 +3,7 @@ import React from 'react'
 const HomePage = () => {
 	return (
 		<div>
-			<div className='jumbotron' id='header_img'>
-			</div>
+			<img id='header_img' src="https://cdn.discordapp.com/attachments/822541048059920435/862335340899074058/meeting-594091_1920.jpg" alt="office space" />
 		</div>
 	)
 }
