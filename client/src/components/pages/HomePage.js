51b5c213a0823b1fdfc089c39a61a9bad3717f6e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-07 17:35:30 +0200

    cleaned up shitty code / Back to start

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const HomePage = () => {
 	return (
 		<div>
-			<img id='header_img' src="https://cdn.discordapp.com/attachments/822541048059920435/862335340899074058/meeting-594091_1920.jpg" alt="office space" />
+		<h1>This is home page</h1>
 		</div>
 	)
 }
