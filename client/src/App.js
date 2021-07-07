4524de6030f59a5db18be23a7d92afb5ac076a90
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-07 17:34:34 +0200

    added box-sizing to entire app

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
 import setAuthToken from './utils/setAuthToken';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 if (localStorage.token) {
